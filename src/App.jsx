c433b6d114d64284a20ef196ddf44747f8b89d62
unknown <nakib15-3358@diu.edu.bd>
2023-03-24 05:38:06 +0600

    project initiallized and navbar done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+
+  return (
+    <div className="max-w-[1440px] mx-auto">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import cartIcon from "../../assets/icons/cart.png";
+import searchIcon from "../../assets/icons/search.png";
+import menuIcon from "../../assets/icons/menu.png";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <>
+      {/* large device */}
+      <div className="mx-14 my-4 hidden lg:flex justify-around items-center">
+        <div className="flex gap-10">
+          <h2 className="font-bold text-base text-[#252B42] leading-8">
+            BrandName
+          </h2>
+          <ul className="flex gap-5 items-center">
+            <li className="font-semibold text-sm text-[#737373]">
+              <a href="#">Home</a>
+            </li>
+            <li className="font-semibold text-sm text-[#737373]">
+              <a href="#">Product</a>
+            </li>
+            <li className="font-semibold text-sm text-[#737373]">
+              <a href="#">Pricing</a>
+            </li>
+            <li className="font-semibold text-sm text-[#737373]">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-11 items-center">
+          <h4 className="font-bold text-sm text-[#23A6F0]">Login</h4>
+          <div className="bg-[#23A6F0] rounded-md px-6 py-4 text-white">
+            Become a member
+            <img src="../../assets/icons/arrow.png" alt="" />
+          </div>
+        </div>
+      </div>
+      {/* small device */}
+      <div className="block lg:hidden">
+        <div className="m-9 flex gap-6 justify-around items-center">
+          <h2 className="font-bold text-base leading-8 text-[#252B42]">
+            BrandName
+          </h2>
+          <div className="flex items-center gap-6">
+            <img src={searchIcon} alt="search" />
+            <img src={cartIcon} alt="cart" />
+            <img
+              src={menuIcon}
+              alt="menu"
+              onClick={() => setShowMenu(!showMenu)}
+            />
+          </div>
+        </div>
+        {showMenu && (
+          <div className="flex flex-col gap-7 justify-center items-center">
+            <h1 className="text-3xl leading-[45px] text-[#737373]">
+              <a href="#">Home</a>
+            </h1>
+            <h1 className="text-3xl leading-[45px] text-[#737373]">
+              <a href="#">Product</a>
+            </h1>
+            <h1 className="text-3xl leading-[45px] text-[#737373]">
+              <a href="#">Pricing</a>
+            </h1>
+            <h1 className="text-3xl leading-[45px] text-[#737373]">
+              <a href="#">Contact</a>
+            </h1>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
